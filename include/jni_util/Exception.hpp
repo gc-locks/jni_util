6996c0b982c27e7257c8152bcc549d0678b4600d
GC <yuuki.inoue@link-u.co.jp>
2020-02-17 14:40:01 +0900

    修正

--- include/jni_util/Exception.hpp
+++ include/jni_util/Exception.hpp
@@ -7,40 +7,38 @@
 #include <exception>
 #include <stdexcept>
 
-namespace jni_util
-{
+namespace jni_util {
 
-class JavaException : public std::exception { };
+    class JavaException : public std::exception {
+    };
 
-inline void throwRuntimeException(JNIEnv* env, const char* what)
-{
-    jclass classj = env->FindClass("Ljava/lang/RuntimeException:");
-    if (classj == nullptr)
-        return;
-    
-    env->ThrowNew(classj, what);
-    env->DeleteLocalRef(classj);
-}
+    inline void throwRuntimeException(JNIEnv *env, const char *what) {
+        jclass classj = env->FindClass("java/lang/RuntimeException");
+        if (classj == nullptr)
+            return;
 
-template <typename Func>
-inline void convertException(JNIEnv* env, Func func) noexcept
-{
-    try {
-        func();
-    } catch (JavaException& e) {
-        return;
-    } catch (std::exception& e) {
-        throwRuntimeException(env, e.what());
-    } catch (...) {
-        throwRuntimeException(env, "Unindentified exception thrown (not derived from std::exception)");
+        env->ThrowNew(classj, what);
+        env->DeleteLocalRef(classj);
     }
-}
 
-inline void checkException(JNIEnv* env)
-{
-    if (env->ExceptionOccurred())
-        throw JavaException();
-}
+    template<typename Func>
+    inline void convertException(JNIEnv *env, Func func) noexcept {
+        try {
+            func();
+        } catch (JavaException &e) {
+            return;
+        } catch (std::exception &e) {
+            throwRuntimeException(env, e.what());
+        } catch (...) {
+            throwRuntimeException(env,
+                                  "Unindentified exception thrown (not derived from std::exception)");
+        }
+    }
+
+    inline void checkException(JNIEnv *env) {
+        if (env->ExceptionOccurred())
+            throw JavaException();
+    }
 
 }
 
